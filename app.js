8b4691c58d6801529acefe8ecd7af34006985bea
florine <florinepltg64@gmail>
2021-03-06 17:22:02 +0100

    initial commit

---
+++ app.js
@@ -0,0 +1,52 @@
+if (process.env.NODE_ENV !== 'production') require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const morgan = require('morgan');
+const favicon = require('serve-favicon');
+const app = express();
+const path = require('path');
+const serveStatic = require('serve-static');
+
+const url = process.env.MONGODB_URI;
+mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true  });
+const db = mongoose.connection;
+mongoose.set('useFindAndModify', false);
+mongoose.set('useCreateIndex', true);
+db.once('open', _ => {
+    console.log('Database connected')
+});
+db.on('error', err => {
+    console.error('connection error:', err)
+});
+app.use(bodyParser.json());
+app.use(morgan('dev'));
+
+app.use(cors());
+
+//routes accessible without being authenticated are redirected in routes
+//app.use("/api", require('./server/routes/publicRoutes'));
+
+
+//All routes with retricted content pass trough the isAuth middleware to verify authentication
+// app.use("/api", require('./server/middleware/auth'), require('./server/routes/privateRoutes'));
+
+/* Handling errors */
+app.use((req,res,next) => {
+    const err = new Error('Not found');
+    err.status = 404;
+    next(err);
+});
+
+app.use((err,req,res,next) => {
+    res.status(err.status || 500);
+    res.json({
+        error : {
+            message : err.message,
+            status : err.status
+        }
+    });
+});
+
+module.exports = app;
