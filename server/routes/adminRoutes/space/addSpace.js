3d75832c152da3732da68f425b1382b33dda23cd
FlorianAudiger <audiger.florian@gmail.com>
2021-03-11 21:10:51 +0100

    update route space

--- server/routes/adminRoutes/space/addSpace.js
+++ server/routes/adminRoutes/space/addSpace.js
@@ -3,7 +3,7 @@ const SpaceController = require('../../../controllers/spaceController');
 module.exports = async (req, res) => {
     try{
 
-        const space = await SpaceController.createSpace(req.body.name,req.body.numberOfTable,req.body.unitPriceOfTable,req.body.m2Price,req.body.festivalId);
+        const space = await SpaceController.createSpace(req.body.name,req.body.numberOfTable1,req.body.unitPriceOfTable1,req.body.m2Price1,req.body.numberOfTable2,req.body.unitPriceOfTable2,req.body.m2Price2,req.body.numberOfTable3,req.body.unitPriceOfTable3,req.body.m2Price3,req.body.festivalId);
 
         if(spaceAdded){
             return res.status(200).json(space);
