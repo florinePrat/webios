31dff67d4b15da1b90bfc0df5e4837f2588c0b20
Timi <ti.mi.34@hotmail.fr>
2021-03-22 22:10:52 +0100

    small fix

--- server/routes/adminRoutes/exhibitor/index.js
+++ server/routes/adminRoutes/exhibitor/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.post('/add', require("./addExhibitor"));
-router.put('/update', require("./updateExibitor" ));
+router.put('/update', require("./updateExhibitor" ));
 router.delete('/delete', require("./deleteExhibitor"));
 
 module.exports =  router;
