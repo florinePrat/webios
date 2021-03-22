ff44a9b332f3a45fe845e3f94ae5fb96bf8fecef
florine <florinepltg64@gmail>
2021-03-22 20:07:11 +0100

    fix

--- server/routes/adminRoutes/exhibitor/index.js
+++ server/routes/adminRoutes/exhibitor/index.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/', require("../../organizerRoutes/exhibitor/getExhibitorByCurrentFestival"));
 router.post('/add', require("./addExhibitor"));
 router.put('/update', require("./updateExibitor" ));
 router.delete('/delete', require("./deleteExhibitor"));
