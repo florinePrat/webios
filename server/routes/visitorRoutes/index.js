8bce367f275bf41956acfd5e2bc0a011d1c48615
florine <florinepltg64@gmail>
2021-03-22 19:47:14 +0100

    add provisori route for add admin

--- server/routes/visitorRoutes/index.js
+++ server/routes/visitorRoutes/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
+router.post('/signupAdmin', require("./signup"));
 router.use('/festival', require("./festival"));
 router.use('/gameBooking', require("./gameBooking"));
 
