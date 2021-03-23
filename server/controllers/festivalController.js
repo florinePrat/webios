a7bbd7dc0e273e509f4cdcd9864a5f75481da93c
florine <florinepltg64@gmail>
2021-03-23 22:33:23 +0100

    populate route

--- server/controllers/festivalController.js
+++ server/controllers/festivalController.js
@@ -20,7 +20,7 @@ const getAllFestival = async() => {
 
 const getCurrentFestival = async() => {
     try {
-        return await Festival.findOne({current : true});
+        return await Festival.findOne({current : true}).populate('space').populate('zoneId');
     } catch (error) {
         console.log(error);
         throw error;
