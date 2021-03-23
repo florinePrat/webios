adc4837d9c4531e1286c1bd36f6a6f4bde9aec45
florine <florinepltg64@gmail>
2021-03-23 12:04:11 +0100

    patch game

--- server/routes/organizerRoutes/game/addGame.js
+++ server/routes/organizerRoutes/game/addGame.js
@@ -3,7 +3,7 @@ const ExhibitorController = require('../../../controllers/exhibitorController');
 
 module.exports = async (req, res) => {
     try{
-        const game = await GameController.createGame(req.body.name,req.body.ageMin,req.body.duration,req.body.category,req.body.prototype,req.body.nbPlayersMin,req.body.nbPlayersMax,req.body.description,req.body.publisherId);
+        const game = await GameController.createGame(req.body.name,req.body.ageMin,req.body.duration,req.body.nbPlayersMin,req.body.nbPlayersMax,req.body.category,req.body.notice,req.body.description,req.body.publisherId, req.body.prototypeGame);
         const gameAdded =  await ExhibitorController.addGame(game._id, req.body.publisherId);
         if(gameAdded){
             return res.status(200).json(game);
