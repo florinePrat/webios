572202b730710d9414d18a6cd3290a2d538ed27f
florine <florinepltg64@gmail>
2021-03-24 15:51:31 +0100

    patch

--- server/controllers/festivalController.js
+++ server/controllers/festivalController.js
@@ -20,7 +20,7 @@ const getAllFestival = async() => {
 
 const getCurrentFestival = async() => {
     try {
-        return await Festival.findOne({current : true}).populate('space').populate('zoneId').populate({path : 'gameBookedList', populate: {path : 'gameId'}});
+        return await Festival.findOne({current : true}).populate('space').populate('zoneId').populate({path : 'gameBookedList', populate: [{path : 'gameId'}, {path : 'zone'}, {path : 'exhibitorId'}]});
     } catch (error) {
         console.log(error);
         throw error;
