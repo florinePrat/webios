99ad8534b5e93a044a5f9444826f1553f9ed53bd
Timi <ti.mi.34@hotmail.fr>
2021-03-28 22:18:44 +0200

    little fix

--- server/routes/adminRoutes/suivi/index.js
+++ server/routes/adminRoutes/suivi/index.js
@@ -2,8 +2,8 @@ const express = require('express');
 const router = express.Router();
 
 // TODO : à tester Postman
-router.post('/update', require("./updateSuivi"));
+router.put('/update', require("./updateSuivi"));
 router.post('/add', require("./addSuivi"));
-router.post('/delete', require("./deleteSuivi"));
+router.delete('/delete', require("./deleteSuivi"));
 
 module.exports =  router;
