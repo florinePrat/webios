84cb3c154e0cd08563beac7339a386508fd11c09
FlorianAudiger <audiger.florian@gmail.com>
2021-03-31 11:11:25 +0200

    add Filtre statut

--- server/controllers/festivalController.js
+++ server/controllers/festivalController.js
@@ -29,7 +29,55 @@ const getCurrentFestival = async() => {
 
 const getExhibitorsByFestivalId = async(festivalId) => {
     try {
-        return await Festival.findById(festivalId).populate({path : 'exhibitors', populate : [{path :  'suiviId'}, {path :  'mainContact'}, {path :  'contacts'}, {path :  'booking'},{path :  'gameList'},{path :  'gameBookedList'}]});
+        const FestivalR = await Festival.findById(festivalId).populate({path : 'exhibitors', populate : [{path :  'suiviId'}, {path :  'mainContact'}, {path :  'contacts'}, {path :  'booking'},{path :  'gameList'},{path :  'gameBookedList'}]});
+        
+        console.log("RESULTAT")
+        
+        let tableaufiltre = new Array(7)
+        let tableauFinal = []
+        for (i=0; i < tableaufiltre.length; i++){
+            tableaufiltre[i] = new Array()
+        }
+
+        for(let i = 0; i < FestivalR.exhibitors.length; i++){
+            const statut = FestivalR.exhibitors[i].suiviId.statusTraking
+            switch (statut) {
+                case 'en discussion':
+                    tableaufiltre[0].push(FestivalR.exhibitors[i])
+                  break;
+                case 'liste de jeux demandée':
+                    tableaufiltre[1].push(FestivalR.exhibitors[i])
+                  break;
+                case 'liste de jeux reçue':
+                    tableaufiltre[2].push(FestivalR.exhibitors[i])
+                  break;
+                case 'pas de réponse':
+                    tableaufiltre[3].push(FestivalR.exhibitors[i])
+                  break;
+                case 'présent':
+                    tableaufiltre[4].push(FestivalR.exhibitors[i])
+                  break;
+                case 'absent':
+                    tableaufiltre[5].push(FestivalR.exhibitors[i])
+                  break;
+                default:
+                    tableaufiltre[6].push(FestivalR.exhibitors[i])
+                }
+            }
+
+        //console.log(tableaufiltre)
+        let k = 0;
+        for (i=0; i < tableaufiltre.length; i++){
+            for(j=0; j < tableaufiltre[i].length; j++){
+                tableauFinal[k] = tableaufiltre[i][j]
+                k++
+            }
+        }
+        console.log(tableauFinal)
+        return tableauFinal;
+
+        //console.log(FestivalR.exhibitors)
+        //return FestivalR
     } catch (error) {
         console.log(error);
         throw error;
